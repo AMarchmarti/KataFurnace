45055caa737e4a6bb451a9cc030a797400e1fc05
AMarchmarti <marchmarti.antoni@gmail.com>
2019-04-03 18:20:48 +0200

    Create interface heateble and class heater

---
+++ src/main/java/org/lasencinas/Aplication/App.java
@@ -0,0 +1,27 @@
+package org.lasencinas.Aplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    final double minTemp = 15.0;
+    final double maxTemp = 21.0;
+/*
+    RoomTemperature temperature = new RoomTemperature(15);
+    Heater heater = new GasHeater();
+    Thermometer thermometer = new RemoteCommandSensor();
+
+    Regulator regulator = new Regulator();
+
+        System.out.println( "Arrancando..." );
+        regulator.regulate(thermometer, heater, minTemp, maxTemp, temperature);
+
+    Jedi yoda = new Jedi();
+        System.out.println( "\nArrancando a Yoda: " );
+        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
+        yoda.speak();*/
+}
+
+
